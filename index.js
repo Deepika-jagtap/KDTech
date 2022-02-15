b8db2c8e558f4ef5509d7f34bc6a234f381713c7
Deepika-jagtap <93381876+Deepika-jagtap@users.noreply.github.com>
2022-02-15 17:11:30 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+let student = [
+    {
+        name: 'deepika',
+        class: "12th",
+        english: 80,
+        Hindi: 80,
+        Maths: 80,
+        
+    },
+    {
+        name: 'kush',
+        class: "11th",
+        english: 40,
+        Hindi: 50,
+        Maths: 44,
+      
+    },
+    {
+        name: "domru",
+        class: "12th",
+        english: 43,
+        Hindi: 55,
+        Maths: 45,
+    
+    },
+]
+for(let i =0; i<=student.lenght-1;i++){
+    if(student[i].name=='deepika'){
+        console.log(i)
+    }
+}
+ 
+    
+
